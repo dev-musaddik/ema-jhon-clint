0ac35ca9296e966a7b2e9bf85ec552c72d12aea7
Your Name <youremail@example.com>
2023-10-13 10:04:58 +0600

    test

--- src/ContexApi/myState.jsx
+++ src/ContexApi/myState.jsx
@@ -1,22 +1,37 @@
-import React, { useState } from 'react'
-import MyContext from './myContex'
-import ProductData from '../componrnts/fakeData'
+import React, { useState } from "react";
+import MyContext from "./myContex";
+import ProductData from "../componrnts/fakeData";
 function MyState(props) {
   const [sortedProducts, setSortedProducts] = useState([...ProductData]);
   const [sortedByPrice, setSortedByPrice] = useState([...ProductData]);
-  const [topPrice,setTopPrice]=useState(false)
-  
-    const state = {
-        name: "Kamal Nayan",
-        class: "9 C"
-    }
-    const [topSell,setTopSell]=useState(false)
-   
+  const [topPrice, setTopPrice] = useState(false);
+  const [search, setSearch] = useState("")
+
+  const state = {
+    name: "Kamal Nayan",
+    class: "9 C",
+  };
+  const [topSell, setTopSell] = useState(false);
+
   return (
-    <MyContext.Provider value={{ state, topSell, setTopSell ,sortedProducts, setSortedProducts,topPrice,setTopPrice,sortedByPrice, setSortedByPrice}}>
-       {props.children} 
+    <MyContext.Provider
+      value={{
+        state,
+        topSell,
+        setTopSell,
+        sortedProducts,
+        setSortedProducts,
+        topPrice,
+        setTopPrice,
+        sortedByPrice,
+        setSortedByPrice,
+        search, 
+        setSearch
+      }}
+    >
+      {props.children}
     </MyContext.Provider>
-  )
+  );
 }
 
-export default MyState
+export default MyState;
--- src/componrnts/Navbar/Navbar.jsx
+++ src/componrnts/Navbar/Navbar.jsx
@@ -1,11 +1,13 @@
-import React from "react";
-import './Navbar.css'
-import logo from './logo.png'
-import {AiOutlineShoppingCart} from 'react-icons/ai'
+import React, { useContext } from "react";
+import "./Navbar.css";
+import logo from "./logo.png";
+import { AiOutlineShoppingCart } from "react-icons/ai";
 import { Link } from "react-router-dom";
-const Navbar = ({numberOfCartItems}) => {
-  
-  
+import myContext from "../../ContexApi/myContex";
+const Navbar = ({ numberOfCartItems }) => {
+  const { search, setSearch } = useContext(myContext);
+  console.log(search)
+
   return (
     <div className="Navbar ">
       <div className="navbar-section d-flex  bg-secondary align-items-center ">
@@ -14,7 +16,9 @@ const Navbar = ({numberOfCartItems}) => {
         </div>
         <div className="topic d-flex  ">
           <ul className="d-flex justify-content-between m-0  list-unstyled">
-            <li><Link to='/' >Shop</Link></li>
+            <li>
+              <Link to="/">Shop</Link>
+            </li>
             <li>Order Review</li>
           </ul>
         </div>
@@ -22,17 +26,21 @@ const Navbar = ({numberOfCartItems}) => {
       </div>
       <div className="search-section bg-info d-flex p-3 ">
         <div className="input w-75 d-flex justify-content-end">
-        <input type="text" className="w-75"/>
-        <button>Search</button>
-        </div>
-        
-        <div className="icon w-25 d-flex justify-content-end align-items-center">
-            <Link to='/cartOverView'><AiOutlineShoppingCart color="orange" size={40}/>
-            <span className="value">{numberOfCartItems}</span>
-            </Link>
-           
+          <input
+            type="text"
+            className="w-75 text-dark pl-2"
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+          />
+          <Link class="text-decoration-none text-white h-100" to='/' ><button className="btn-primary pl-2 pr-2 h-100">Search</button></Link>
         </div>
 
+        <div className="icon w-25 d-flex justify-content-end align-items-center">
+          <Link to="/cartOverView">
+            <AiOutlineShoppingCart color="orange" size={40} />
+            <span className="value">{numberOfCartItems}</span>
+          </Link>
+        </div>
       </div>
     </div>
   );
